3008edc42cb2d7b8c2f7bccab1d12604a68ed4db
Rita Rasteiro <rita.rasteiro@gmail.com>
2023-02-15 11:42:20 +0000

    import gwasglue objects

--- R/hyprcoloc.R
+++ R/hyprcoloc.R
@@ -956,6 +956,8 @@ rapid.hyprcoloc <- function(Zsq, Wsq, prior.1, prior.2, uniform.priors){
 #' # Colocalisation analyses
 #' results <- hyprcoloc(betas, ses, trait.names=traits, snp.id=rsid)
 #' @export
+#' @importFrom gwasglue2 dataset_to_hyprcoloc
+#' @importClassesFrom gwasglue2 DataSet
   hyprcoloc <- function(effect.est = NULL, effect.se = NULL, binary.outcomes = rep(0, dim(effect.est)[2]), 
                         trait.subset = c(1:dim(effect.est)[2]), trait.names = c(1:dim(effect.est)[2]),
                         snp.id = c(1:dim(effect.est)[1]), ld.matrix = diag(1, dim(effect.est)[1], dim(effect.est)[1]),
